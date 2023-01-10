d2f8ce78a96ffc7a89a95d201a6a7ac90c35a27f
Rodrigo-Eira <baiaeira@gmail.com>
2023-01-10 17:46:33 +0000

    olua

--- diario/models.py
+++ diario/models.py
@@ -17,7 +17,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 #     class Meta:
 #         abstract = True
-
+#         Eu CONSEGUI
 
 class Reference(models.Model):
     nome = models.CharField(max_length=20, default="")
