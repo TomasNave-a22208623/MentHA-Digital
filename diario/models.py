0914d00f8b4f9099a2da70073ebcf8c7b5d99835
Alexandre-Godinho <alexferreiragodinho@gmail.com>
2023-01-10 18:01:53 +0000

    Da-lhe Zé

--- diario/models.py
+++ diario/models.py
@@ -20,6 +20,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 #         Eu CONSEGUI
 #         EU JAMES TAMBEM
 #         O Tiago Silva esteve aqui
+#         Da-lhe Zé
 
 class Reference(models.Model):
     nome = models.CharField(max_length=20, default="")
