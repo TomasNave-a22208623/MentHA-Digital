4ca6e58b23cf9c768bfad7473300dbc3529e4bdf
Lucio Studer <luciostuder@Lucios-iMac.local>
2023-07-11 21:35:10 +0100

    margem do mentha.logo igual entre dashboard e base

--- diario/models.py
+++ diario/models.py
@@ -26,6 +26,12 @@ class Avaliador(models.Model):
                             null=True)
     reference = models.ForeignKey(Reference, on_delete=models.CASCADE, null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.user}'
+    
+
+
+
 class Doenca(models.Model):
     nome = models.CharField(max_length=20, default="")
 
